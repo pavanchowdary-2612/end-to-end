c5aafdac2764d34a8c362ab54d33256e77fd154e
pavan <pavanchowdary2612@gmail.com>
2024-12-14 01:49:23 +0530

    adding

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import find_packages ,setup
-# from typing import List
+from typing import List
+
+
+def get_requirements()->list[str]:
+    
+    requirements_list = list[str]=[]
+
+    return requirements_list
+
 
 setup(
 name = 'sensor',
@@ -7,6 +15,8 @@ version = "0.0.1",
 author = "prince",
 author_email="k.pkc2612@gmail.com",
 packages = find_packages(),
-install_requires = ["pymongo"]
+install_requires = get_requirements(),#["pymongo"]
 
 )
+
+
